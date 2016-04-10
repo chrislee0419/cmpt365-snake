a37824587eca4768fbd16835239e268e87d95270
Matthew <tsengmatthew@hotmail.com>
2016-04-09 20:45:46 -0700

    added some functions for Game.h and Game.cpp

--- Snake/objects/game/Game.cpp
+++ Snake/objects/game/Game.cpp
@@ -13,9 +13,90 @@
 	- black background
 
 	- game blocks = 39 x 29
+	- block size = 20 x 20
 
 	- player head is red
 	- player body is yellow
 
 
 */
+
+#include "Game.h"
+#include "..\test.h"
+#include "..\_util.h"
+
+using namespace std;
+
+// Globals 
+
+// Window size
+float xsize = 800.0;
+float ysize = 600.0;
+
+glm::vec2 tilepos = glm::vec2(20, 15);
+
+// board[x][y] represents whether the cell (x, y) is occupied
+bool board[39][29];
+
+// Constructor
+Game::Game()
+{
+	Init();
+}
+
+// Destructor
+Game::~Game()
+{
+
+}
+
+void Game::Init()
+{
+	_InitBoard();
+	_InitStart();
+}
+
+void Game::Move(glm::vec2 direction)
+{
+
+}
+
+// Private helper methods
+void Game::_InitBoard()
+{
+	Box *board_init = (Box*)malloc(sizeof(Box) * (29 * 39));
+	int k = 0;
+
+	for (int i = 0; i < 29; i++)
+	{
+		for (int j = 0; j < 39; j++)
+		{
+			board_init[k] = Box(20, 20, 20 * j, 20 * i, BLACK, BLACK);
+			k++;
+		}
+	}
+
+	for (int i = 0; i < 29; i++)
+		for (int j = 0; j < 39; j++)
+			board[i][j] = false;
+
+	for (int i = 0; i < 29 * 39; i++)
+		board_init[i].Draw(0, 0);
+}
+
+void Game::_InitStart()
+{
+	Box *snake_init = (Box*)malloc(sizeof(Box) * 1);
+	snake_init[0] = Box(20, 20, 20 * tilepos[0], 20 * tilepos[1], RED, RED);
+	snake_init[0].Draw(0, 0);
+}
+
+//void Test::_CreateGameTest()
+//{
+//
+//}
+//
+//void Test::_DisplayGameTest()
+//{
+//
+//}
--- Snake/objects/game/Game.h
+++ Snake/objects/game/Game.h
@@ -3,3 +3,32 @@
 //	by Chris Lee - cla235
 */
 
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+
+#include "..\..\depend\glew\glew.h"
+#include "..\..\depend\freeglut\freeglut.h"
+#include "..\..\shaders\Shader.h"
+#include "..\..\depend\glm\vec4.hpp"
+#include "..\_colours.h"
+
+class Game
+{
+private:
+	bool _ready;
+
+	void _Init();
+	void _InitBoard();
+	void _InitStart();
+	bool _ready;
+
+public:
+	Game();
+	~Game();
+
+	void Restart();
+	void Move(glm::vec2 direction);
+	void Init();
+};
